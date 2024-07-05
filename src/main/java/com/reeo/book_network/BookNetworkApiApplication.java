c6885895689171a7585efc2a355826aa1a79f37c
muhammad-rio <muhammad.rioperm@gmail.com>
2024-07-05 19:20:22 +0700

    Setup initial configuration

--- src/main/java/com/reeo/book_network/BookNetworkApiApplication.java
+++ src/main/java/com/reeo/book_network/BookNetworkApiApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookNetworkApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BookNetworkApiApplication.class, args);
+  }
 
 }
