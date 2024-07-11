04e36c2d15c9d8515a4b195c49c59534448d7f6a
muhammad-rio <muhammad.rioperm@gmail.com>
2024-07-11 16:48:24 +0700

    Add OpenAPI configuration

---
+++ backend/src/main/java/com/reeo/book_network/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package com.reeo.book_network.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        contact = @Contact(
+            name = "Muhammad Rio",
+            email = "muhrio.perm@gmail.com",
+            url = "https://twitter.com/RyoPermana33"
+        ),
+        description = "OpenApi documentation for spring security",
+        title = "OpenAPI specification - Rio",
+        version = "1.0.0",
+        license = @License(
+            name = "MIT-License",
+            url = "https://www.mit.edu/~amini/LICENSE.md"
+        ),
+        termsOfService = "Term of service"
+    ),
+    servers = {
+        @Server(
+            description = "Local ENV",
+            url = "http://localhost:8080/api/v1"
+        ),
+        @Server(
+            description = "Prod ENV",
+            url = "https://example.com/"
+        )
+    },
+    security = {
+        @SecurityRequirement(
+            name = "BearerAuth"
+        )
+    }
+)
+@SecurityScheme(
+    name = "BearerAuth",
+    description = "JWT auth security",
+    scheme = "Bearer",
+    type = SecuritySchemeType.HTTP,
+    in = SecuritySchemeIn.HEADER,
+    bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
