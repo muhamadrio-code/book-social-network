cca662ac237469518b5bdad478f099dd4912339c
muhammad-rio <muhammad.rioperm@gmail.com>
2024-07-19 11:19:57 +0700

    ✨ Migrating to keycloack

--- backend/src/main/java/com/reeo/book_network/email/MailService.java
+++
@@ -1,9 +0,0 @@
-package com.reeo.book_network.email;
-
-import jakarta.mail.MessagingException;
-import org.springframework.scheduling.annotation.Async;
-
-public interface MailService<T> {
-  @Async
-  void sendMail(T mailRequest) throws MessagingException;
-}
